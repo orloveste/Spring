cbacfa58f17c9ff6cbffafb1dd3434803cae7934
orloveste <orlovevm@yahoo.com>
2020-11-08 15:54:57 +0200

    get save rest

--- src/main/java/myApp/controlers/PersonController.java
+++ src/main/java/myApp/controlers/PersonController.java
@@ -1,10 +1,7 @@
 package myApp.controlers;
 
 import myApp.controlers.jsons.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,5 +31,10 @@ public class PersonController {
         person.setCnp(id);
         person.setName("persoana cu id");
         return person;
+    }
+
+    @PutMapping
+    public void save(@RequestBody Person person) {
+        System.out.println("save this " + person);
     }
 }
