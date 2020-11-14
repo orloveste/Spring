89fdf2c2cb816f3959a4366d31a66bff6ccd379c
orloveste <orlovevm@yahoo.com>
2020-11-14 15:42:43 +0200

    injectie pe constructor - simplificat

--- src/main/java/myApp/controlers/HelloWorldController.java
+++ src/main/java/myApp/controlers/HelloWorldController.java
@@ -1,18 +1,17 @@
 package myApp.controlers;
 
 import myApp.spring.Singletone;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @Autowired
-    Singletone singletone;
+
+    private Singletone singletone;
 
     //injectie la nivel de constructor dependinte
-    public HelloWorldController(@Autowired Singletone singletone) {
+    public HelloWorldController(Singletone singletone) {
         this.singletone = singletone;
     }
 
