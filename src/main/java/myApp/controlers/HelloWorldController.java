1c47063cf58535fe137035cd17549bc4be602564
orloveste <orlovevm@yahoo.com>
2020-11-14 15:46:46 +0200

    injectie pe constructor - simplificat cu lombok

--- src/main/java/myApp/controlers/HelloWorldController.java
+++ src/main/java/myApp/controlers/HelloWorldController.java
@@ -1,19 +1,21 @@
 package myApp.controlers;
 
+import lombok.RequiredArgsConstructor;
 import myApp.spring.Singletone;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
 
 
-    private Singletone singletone;
+    final private Singletone singletone;
 
     //injectie la nivel de constructor dependinte
-    public HelloWorldController(Singletone singletone) {
-        this.singletone = singletone;
-    }
+//    public HelloWorldController(Singletone singletone) {
+//        this.singletone = singletone;
+//    }
 
     @GetMapping
     public String hello() {
