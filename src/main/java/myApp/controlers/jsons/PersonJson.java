3110a1abf8d12aeb9391ff651612764b413663a9
orloveste <orlovevm@yahoo.com>
2020-11-15 10:18:34 +0200

    creezi in java tabelul

--- src/main/java/myApp/controlers/jsons/PersonJson.java
+++ src/main/java/myApp/controlers/jsons/PersonJson.java
@@ -2,13 +2,20 @@ package myApp.controlers.jsons;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "persoana")
 public class PersonJson {
     @Id
+    @Column(name = "cnp")
     private Integer cnp;
+
+    @Column(name = "name")
     private String name;
+
 }
