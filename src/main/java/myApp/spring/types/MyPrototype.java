4fa2c18bb4e448206c55f5b86cb95967b63f985a
orloveste <orlovevm@yahoo.com>
2020-11-15 13:57:15 +0200

    container spring - scope

--- src/main/java/myApp/spring/types/MyPrototype.java
+++ src/main/java/myApp/spring/types/MyPrototype.java
@@ -3,7 +3,7 @@ package myApp.spring.types;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Scope("prototype")
+@Scope("prototype")//managed by spring but create new instance
 @Component
 public class MyPrototype {
 
