2aab785473b75876b00ec7bed5a2a452f26747bf
orloveste <orlovevm@yahoo.com>
2020-11-28 15:51:37 +0200

    spring - timers automated comment

--- src/main/java/myApp/spring/timers/MyTimer.java
+++ src/main/java/myApp/spring/timers/MyTimer.java
@@ -7,8 +7,8 @@ import java.util.Date;
 
 @Component
 public class MyTimer {
-    @Scheduled(fixedRate = 1000)
-    public void reportCurrentTime() {
+    @Scheduled(fixedRate = 1000) //trigger
+    public void reportCurrentTime() {//business trebuie executat candva automat
         System.out.println(this + " stupid timer was triggered at " + new Date());
     }
 }
