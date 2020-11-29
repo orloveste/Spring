0f3f1822ba6be84d68ce43119327da95ea6053b2
orloveste <orlovevm@yahoo.com>
2020-11-29 14:50:17 +0200

    spring boot -REST http://localhost:8080/person debug

--- src/main/java/myApp/controlers/PersonController.java
+++ src/main/java/myApp/controlers/PersonController.java
@@ -5,6 +5,8 @@ import myApp.controlers.jsons.PersonJson;
 import myApp.services.PersonService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -15,8 +17,8 @@ public class PersonController {
 //http://localhost:8080/person Json
     final private PersonService personService;
     @GetMapping
-    public List<PersonJson> findAll() {
-        return personService.findAll();
+    public List<PersonJson> findAll(HttpServletRequest request, HttpServletResponse response) {
+        return personService.findAll();//debug and see
     }
     @GetMapping("/{id}")
     public PersonJson findById(@PathVariable("id") Integer id) {
