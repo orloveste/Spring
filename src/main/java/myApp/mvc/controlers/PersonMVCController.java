5681a5ba5e83f0f534f91d9cebefc68c170572db
orloveste <orlovevm@yahoo.com>
2020-11-29 12:13:18 +0200

    spring boot and thymeleaf - http://localhost:8080/ persons

---
+++ src/main/java/myApp/mvc/controlers/PersonMVCController.java
@@ -0,0 +1,33 @@
+package myApp.mvc.controlers;
+
+import myApp.controlers.jsons.PersonJson;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/persons")
+public class PersonMVCController {
+    @GetMapping(value = "list")
+    public String listPersons(Model model) {//parte din semnatura de metoda - model
+        List<PersonJson> list = new ArrayList<>();
+        {
+            PersonJson personJson = new PersonJson();
+            personJson.setCnp(1);
+            personJson.setName("ion");
+            list.add(personJson);
+        }
+        {
+            PersonJson personJson = new PersonJson();
+            personJson.setCnp(2);
+            personJson.setName("ghe");
+            list.add(personJson);
+        }
+        model.addAttribute("persons", list);
+        return "persons";//textul returnat trebuie sa fie un nume de template - match cu nume de fisier din folder de template
+    }
+}
