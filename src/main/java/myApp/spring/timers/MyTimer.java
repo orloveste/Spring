e83426e84ad9ec6cbf4308aef24c4bf9b8cfa508
orloveste <orlovevm@yahoo.com>
2020-11-29 10:30:31 +0200

    spring - timers out for app

--- src/main/java/myApp/spring/timers/MyTimer.java
+++ src/main/java/myApp/spring/timers/MyTimer.java
@@ -1,13 +1,12 @@
 package myApp.spring.timers;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class MyTimer {
-    @Scheduled(fixedRate = 1000) //trigger
+//    @Scheduled(fixedRate = 10000) //trigger
     public void reportCurrentTime() {//business trebuie executat candva automat
         System.out.println(this + " stupid timer was triggered at " + new Date());
     }
