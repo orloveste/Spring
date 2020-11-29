0ec294c2b6257500271b94e9261cafc1e0945c6a
orloveste <orlovevm@yahoo.com>
2020-11-29 12:42:24 +0200

    spring boot and thymeleaf - http://localhost:8080 - create person

--- src/main/java/myApp/mvc/controlers/PersonMVCController.java
+++ src/main/java/myApp/mvc/controlers/PersonMVCController.java
@@ -3,7 +3,10 @@ package myApp.mvc.controlers;
 import myApp.controlers.jsons.PersonJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -30,5 +33,17 @@ public class PersonMVCController {
         model.addAttribute("persons", list);//controller-ul gestioneaza modelul si view-ul care trebui desenat
 
         return "persons";//textul returnat trebuie sa fie un nume de template - match cu nume de fisier din folder de template
+    }
+    @GetMapping(value = "/createPerson")
+    public String createPerson (Model model){
+        PersonJson personJson = new PersonJson();
+        model.addAttribute("person", personJson);
+        return "personCreate";
+    }
+    @PostMapping(value = "/savePerson")
+    public String savePerson (Model model, @ModelAttribute PersonJson personJson, BindingResult errors){
+        model.addAttribute("person", personJson);
+        System.out.println("saving " + personJson);
+        return "personCreate";
     }
 }
