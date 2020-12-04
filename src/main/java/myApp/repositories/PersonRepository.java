bb3dae28205747a1968ecb68043732decbb33bad
orloveste <orlovevm@yahoo.com>
2020-12-04 15:55:14 +0200

    Not clear

--- src/main/java/myApp/repositories/PersonRepository.java
+++ src/main/java/myApp/repositories/PersonRepository.java
@@ -2,6 +2,7 @@ package myApp.repositories;
 
 import myApp.controlers.jsons.PersonJson;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface PersonRepository extends JpaRepository<PersonJson, Integer> {
 
 }
