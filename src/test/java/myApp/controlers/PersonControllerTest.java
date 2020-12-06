a1ebba27c1d5fc4f1d6997632a66aba66aa251a4
orloveste <orlovevm@yahoo.com>
2020-12-06 14:13:54 +0200

    returnezi o lista goala pe care o testezi - controlezi comportamentul facaturilor - cum controlezi = ca parte la pasul 1 - context date create - asa crapa ca lista nu este goala -  a reusit dar avem probleme in asteptari

--- src/test/java/myApp/controlers/PersonControllerTest.java
+++ src/test/java/myApp/controlers/PersonControllerTest.java
@@ -3,12 +3,15 @@ package myApp.controlers;
 import myApp.controlers.jsons.PersonJson;
 import myApp.services.PersonService;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -17,13 +20,13 @@ public class PersonControllerTest {
     @InjectMocks //ii spune lui mockito ca sa creeze o instanta de person controler
     // care sa aiba o referintacatre person service
     //si-l invoca singur
-    PersonController controller;
+    PersonController targetObject;
 //    {// nu ai nevoie de constructor pentru ca ai injectie pe campuri
 //        controller = new PersonController(mock); // asta este versiunea manuala
 //    }
 
     @Mock //am nevoie de acest personservice pe care il creezi tu
-    PersonService service;
+    PersonService mock;
 //    PersonService service = Mockito.mock(PersonService.class); //creare mock manual
 // surogat care se preface ca este un obiect in personservice
     // cate mock-uri ai le seteaza pe inject daca poate
@@ -31,9 +34,19 @@ public class PersonControllerTest {
     @Test
     public void testFindAll() {
         //1
+        //creare context de testare
+        //1.1
+        //definim mock-urile + comportamentul acestora
+        List<PersonJson> jsons = new ArrayList<>();
+        PersonJson personJson = new PersonJson();
+        jsons.add(personJson);
+        //specific mock-ului sa faca ceva, nu sunt oblicat sa definesc comportament pentru tot
+        //doar la ce ma intereseaza
+        Mockito.when(mock.findAll()).thenReturn(jsons);
         //2
-        ResponseEntity<List<PersonJson>> responseEntity = controller.findAll(null,null);
+        //comportamentul mock-urilor
+        ResponseEntity<List<PersonJson>> responseEntity = targetObject.findAll(null,null);
         //3
-        System.out.println(responseEntity.getBody());
+        Assertions.assertTrue(responseEntity.getBody().isEmpty());
     }
 }
